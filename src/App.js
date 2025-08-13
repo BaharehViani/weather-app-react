4aa44c69357a45b9202a249b887af7f821b852c2
bahareh <baharehviani@gmail.com>
2025-08-13 15:58:23 +0330

    footer added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <footer><a href="https://github.com/BaharehViani/weather-app-react">open-source</a> code, by <a href="https://github.com/BaharehViani">Bahareh Viani</a></footer>
     </div>
   );
 }

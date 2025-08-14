4cc4f095f6ca63e7e6f363ed2e10ee80e88dc9d6
bahareh <baharehviani@gmail.com>
2025-08-14 15:17:56 +0330

    unit convertion added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York"/>
-        <footer><a href="https://github.com/BaharehViani/weather-app-react">open-source</a> code, by <a href="https://github.com/BaharehViani">Bahareh Viani</a></footer>
+        <Weather defaultCity="New York" />
+        <footer>
+          <a href="https://github.com/BaharehViani/weather-app-react">
+            open-source
+          </a>{" "}
+          code, by <a href="https://github.com/BaharehViani">Bahareh Viani</a>
+        </footer>
       </div>
     </div>
   );
